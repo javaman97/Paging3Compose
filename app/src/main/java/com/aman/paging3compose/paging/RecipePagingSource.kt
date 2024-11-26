f174bd8957fe772cefac1aa34114b44eba46536b
aman nishad <javaman0512@gmail.com>
2024-11-27 02:27:56 +0530

    Added Logs

--- app/src/main/java/com/aman/paging3compose/paging/RecipePagingSource.kt
+++ app/src/main/java/com/aman/paging3compose/paging/RecipePagingSource.kt
@@ -1,5 +1,6 @@
 package com.aman.paging3compose.paging
 
+import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.aman.paging3compose.api.ApiHelper
@@ -8,6 +9,7 @@ import com.aman.paging3compose.model.RecipeX
 class RecipePagingSource(private val api:ApiHelper):PagingSource<Int, RecipeX> (){
 
     override fun getRefreshKey(state: PagingState<Int, RecipeX>): Int? {
+        Log.e("Paging Source"," GetRefreshKey ${state.anchorPosition}")
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(10) ?:
             state.closestPageToPosition(anchorPosition)?.nextKey?.minus(10)
@@ -20,6 +22,7 @@ class RecipePagingSource(private val api:ApiHelper):PagingSource<Int, RecipeX> (
         val pageSize = params.loadSize
 
         return try {
+            Log.e("Paging Source"," Position = $position and Page Size = $pageSize")
             val response = api.getRecipe(limit = pageSize, skip= position)
             LoadResult.Page(
                 data = response.recipes,
